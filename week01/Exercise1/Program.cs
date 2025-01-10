dd4c492132e20ab76be0ad51eb41819594edf90a
Sembatya2020 <ivansmbt@gmail.com>
2025-01-10 11:22:22 +0300

    Asked names

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("what is your first name?");
+        string FirstName = Console.ReadLine(); 
+        Console.WriteLine("what is your last name?");
+        string LastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {LastName}, {FirstName}, {LastName}.");
     }
 }
